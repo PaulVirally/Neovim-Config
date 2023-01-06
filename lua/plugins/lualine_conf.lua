1079aa8a95c22e85c200b76cf03aa83083951a2e
Paul Virally <pvirally@gmail.com>
2023-01-06 15:32:44 -0500

    Removed buffer names from status line

--- lua/plugins/lualine_conf.lua
+++ lua/plugins/lualine_conf.lua
@@ -7,7 +7,7 @@ require('lualine').setup({
 	sections = {
 		lualine_a = {'mode'},
 		lualine_b = {'branch', 'diff', 'diagnostics'},
-		lualine_c = {'filename', 'buffers'},
+		lualine_c = {'filename'},
 		lualine_x = {'encoding', 'filetype'},
 		lualine_y = {'progress'},
 		lualine_z = {'location'}
