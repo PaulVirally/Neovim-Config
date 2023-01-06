59796cc5282fdaffbdee157a615ca50d8345590b
Paul Virally <pvirally@gmail.com>
2023-01-05 21:25:31 -0500

    Removed indent guides from lazy window

--- lua/plugins/indent_blankline_conf.lua
+++ lua/plugins/indent_blankline_conf.lua
@@ -2,7 +2,7 @@
 
 require('indent_blankline').setup({
 	buftype_exclude = {'terminal'},
-	filetype_exclude = {'help', 'NvimTree', 'TelescopePrompt', 'TelescopeResults'},
+	filetype_exclude = {'help', 'NvimTree', 'TelescopePrompt', 'TelescopeResults', 'lazy'},
 	show_trailing_blankline_indent = false,
 	show_current_context = true,
 })
