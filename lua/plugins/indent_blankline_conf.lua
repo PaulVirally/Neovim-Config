cf3b1e84de93862c05c379e79ffa2f1922d7b569
Paul Virally <pvirally@gmail.com>
2023-01-06 00:40:32 -0500

    Added noice

--- lua/plugins/indent_blankline_conf.lua
+++ lua/plugins/indent_blankline_conf.lua
@@ -1,8 +1,8 @@
 -- Indent blank line config
 
 require('indent_blankline').setup({
-	buftype_exclude = {'terminal'},
-	filetype_exclude = {'help', 'NvimTree', 'TelescopePrompt', 'TelescopeResults', 'lazy'},
+	buftype_exclude = {'terminal', 'fidget'},
+	filetype_exclude = {'help', 'NvimTree', 'TelescopePrompt', 'TelescopeResults', 'lazy', 'fidget'},
 	show_trailing_blankline_indent = false,
 	show_current_context = true,
 })
