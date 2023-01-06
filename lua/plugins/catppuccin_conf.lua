a73c5a946e5cf8fdcb553a6702dceb28adcc16e1
Paul Virally <pvirally@gmail.com>
2023-01-06 00:41:02 -0500

    Added catppuccin config

---
+++ lua/plugins/catppuccin_conf.lua
@@ -0,0 +1,34 @@
+-- Catppuccin config
+
+require('catppuccin').setup({
+	integrations = {
+		fidget = true,
+		gitsigns = true,
+		indent_blankline = {
+			enabled = true,
+			colored_indent_levels = true,
+		},
+		mason = true,
+		-- noice = true,
+		cmp = true,
+		native_lsp = {
+			enabled = true,
+			virtual_text = {
+				errors = {'italic'},
+				hints = {'italic'},
+				warnings = {'italic'},
+				information = {'italic'},
+			},
+			underlines = {
+				errors = {'underline'},
+				hints = {'underline'},
+				warnings = {'underline'},
+				information = {'underline'},
+			},
+		},
+		treesitter = true,
+		telescope = true,
+		lsp_trouble = true,
+		which_key = true,
+	}
+})
