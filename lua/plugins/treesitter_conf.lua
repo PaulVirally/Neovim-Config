e1469a5eeb648dbb2b787821d22f7c6eb09b562d
Paul Virally <pvirally@gmail.com>
2023-01-11 22:47:01 -0500

    Added treesitter playground

--- lua/plugins/treesitter_conf.lua
+++ lua/plugins/treesitter_conf.lua
@@ -2,8 +2,7 @@
 
 require('nvim-treesitter.configs').setup {
 	-- Add languages to be installed here that you want installed for treesitter
-	ensure_installed = {'c', 'cpp', 'latex', 'lua', 'python', 'rust', 'typescript', 'help', 'vim', 'regex', 'bash', 'markdown', 'markdown_inline'},
-
+	ensure_installed = {'c', 'cpp', 'latex', 'lua', 'python', 'rust', 'typescript', 'help', 'vim', 'regex', 'bash', 'markdown', 'markdown_inline', 'query'},
 	highlight = {enable = true},
 	indent = {enable = true, disable = {'python'}},
 	incremental_selection = {
@@ -59,4 +58,22 @@ require('nvim-treesitter.configs').setup {
 			},
 		},
 	},
+	playground = {
+		enable = true,
+		disable = {},
+		updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+		persist_queries = false, -- Whether the query persists across vim sessions
+		keybindings = {
+			toggle_query_editor = 'o',
+			toggle_hl_groups = 'i',
+			toggle_injected_languages = 't',
+			toggle_anonymous_nodes = 'a',
+			toggle_language_display = 'I',
+			focus_language = 'f',
+			unfocus_language = 'F',
+			update = 'R',
+			goto_node = '<cr>',
+			show_help = '?',
+		},
+	}
 }
