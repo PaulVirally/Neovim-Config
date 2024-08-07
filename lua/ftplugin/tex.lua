24f9ccd7c56f4cc03dd37740f46b79a68f90947a
Paul Virally <pvirally@gmail.com>
2024-08-07 10:17:53 -0400

    Fixed comments in latex

--- lua/ftplugin/tex.lua
+++ lua/ftplugin/tex.lua
@@ -3,3 +3,4 @@ require('luasnip').setup({
 	enable_autosnippets = true,
 })
 vim.opt.wrap = true -- Wrap lines in LaTeX
+vim.api.nvim_set_option_value('commentstring', '% %s', {}) -- Set commentstring to LaTeX comment
